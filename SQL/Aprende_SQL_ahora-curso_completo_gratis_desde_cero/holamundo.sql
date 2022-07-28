7a71659bb71f7cd0ce43bb8ef9c1f03655a986c6
CristopherVanegas <cristopher.platzi@gmail.com>
2022-07-28 03:14:21 -0500

    CREATED Animal table and location table

--- SQL/Aprende_SQL_ahora-curso_completo_gratis_desde_cero/holamundo.sql
+++ SQL/Aprende_SQL_ahora-curso_completo_gratis_desde_cero/holamundo.sql
@@ -2,3 +2,18 @@ create database holamundo;
 show databases;
 
 use holamundo;
+CREATE TABLE animales (
+	id int,
+    tipo varchar(255),
+    estado varchar(255),
+    PRIMARY KEY (id)
+);
+
+use holamundo;
+CREATE TABLE location (
+	id int,
+    location varchar(255),
+    animal varchar(255),
+    
+    PRIMARY KEY (id)
+)
