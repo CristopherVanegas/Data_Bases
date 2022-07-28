c34277a4cb1deaa50382841cf21d8614d6372112
CristopherVanegas <cristopher.platzi@gmail.com>
2022-07-28 09:12:22 -0500

    ADDED A COMMENT WITH --
    In line 15: id COLUMN WAS ALTERED FROM TABLE 'animales' AND auto_increment +1 WHEN A NEW DATA IT'S INSERTED.
    
    In line 17: IT'S SHOWN THE COMMAND TO CREATE TABLE 'animales':
    
    'CREATE TABLE `animales` (
      `id` int NOT NULL AUTO_INCREMENT,
      `tipo` varchar(255) DEFAULT NULL,
      `estado` varchar(255) DEFAULT NULL,
      PRIMARY KEY (`id`)
    ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
    
    (The last line it's not neccessary)
    
    In line 19 to 24: IT'S APPLIED THE COMMAND BUT DELETED THE UNECCESSARY LINE,
    	`id` 		IT'S INT AND MUST NOT BE NULL ALSO IT AUTO_INCREMENTS
    	`tipo`		IT'S A 255 VARCHAR AND BY DEFAULT IT'S NULL
    	`estado`	IT'S A 255 VARCHAR AND BY DEFAULT IT'S NULL
    	THEN PRIMARY KEY IT'S SET AS					(`id`)
    
    In line 26 to 28: MANY DATA ARE INSERTED INTO THE TABLE,
    
    	VALUES ('CHANCHITO', 'FELIZ') ARE INSERTED INTO TYPE AND STATUS COLUMNS FROM animales TABLE RESPECTIVELY
    	<the other values follow the same rule>
    
    In line 30 to 32:
    	30.- SELECT ALL COLUMNS FROM animales
    	31.- SELECT ALL COLUMNS FROM animales WHERE `id` = 4
    	32.- SELECT ALL COLUMNS FROM animales WHERE `estado` = 'feliz'

--- SQL/Aprende_SQL_ahora-curso_completo_gratis_desde_cero/holamundo.sql
+++ SQL/Aprende_SQL_ahora-curso_completo_gratis_desde_cero/holamundo.sql
@@ -9,11 +9,24 @@ CREATE TABLE animales (
     PRIMARY KEY (id)
 );
 
-use holamundo;
-CREATE TABLE location (
-	id int,
-    location varchar(255),
-    animal varchar(255),
-    
-    PRIMARY KEY (id)
-)
+-- INSERT INTO animales (tipo, estado) VALUES ('chanchito', 'feliz');
+-- esta línea no será ejecutada
+
+ALTER TABLE animales MODIFY COLUMN  id int auto_increment;
+
+SHOW CREATE TABLE animales;
+
+CREATE TABLE `animales` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(255) DEFAULT NULL,
+  `estado` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO animales (tipo, estado) VALUES ('chanchito', 'feliz');
+INSERT INTO animales (tipo, estado) VALUES ('dragon', 'feliz');
+INSERT INTO animales (tipo, estado) VALUES ('felipe', 'triste');
+
+SELECT * FROM animales;							-- SELECTS ALL COLUMNS FROM animales.
+SELECT * FROM animales WHERE id = 4;			-- SELECTS ALL COLUMNS FROM animales DONDE id ES IGUAL A 4
+SELECT * FROM animales WHERE estado = 'feliz';	-- SELECTS ALL COLUMNS FROM animales DONDE estado ES IGUAL A 'feliz'.
